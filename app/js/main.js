be828041cdb98ebb5f05807af8fe6e8811a921b1
Your Name <you@example.com>
2022-06-08 23:34:03 +0300

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -52,3 +52,35 @@ function tabsContent() {
   });
 }
 tabsContent();
+
+
+
+
+
+
+
+
+  //Mobile Menu
+  const burger = document.querySelector('.burger'); //наша кнопка
+  const mobileMenu = document.querySelector('.menu'); //мобильное меню
+  const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('menu--active'); //когда меню открыто
+    if (mobileMenu.classList.contains('menu--active'))  { //Проверяем, есть ли у меню активный класс
+      burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
+      bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+    }
+    else { //Когда нету активного класса у меню
+      burger.classList.remove('burger--active'); //Возвращает в исходное состояние
+      bodyLock.classList.remove('lock'); //Разрешаем скроллить
+    }
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      burger.classList.remove('burger--active');
+      mobileMenu.classList.remove('menu--active');
+      bodyLock.classList.remove('lock');
+    }
+  });
